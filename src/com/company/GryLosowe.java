f11ed51c37b28b1c2f223b69265cbc756b1ef110
Magda Podgorska <podma@wp.pl>
2018-04-25 17:16:40 +0200

    Gry losowe poprawka

---
+++ src/com/company/GryLosowe.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import static java.lang.Thread.sleep;
+
+public class GryLosowe {
+    public static void main(String[] args) throws InterruptedException {
+
+        Totek start=new Totek();
+        start.wstep();
+
+        sleep(2000);
+
+        Totek losowanie=new Totek();
+        losowanie.losuj();
+        losowanie.sort();
+        losowanie.wypisz();
+
+        sleep(2000);
+
+        ExtraPensja start1=new ExtraPensja();
+        start1.wstep();
+
+        sleep(2000);
+
+        ExtraPensja losowanie1=new ExtraPensja();
+        losowanie1.losuj();
+        losowanie1.sort();
+        losowanie1.wypisz();
+        sleep(2000);
+        losowanie1.dodatkowy();
+        losowanie1.losuj1();
+        sleep(2000);
+        losowanie1.wypisz1();
+
+
+    }
+}
