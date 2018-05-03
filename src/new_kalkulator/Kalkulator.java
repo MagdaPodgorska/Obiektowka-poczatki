467dc68be97cfdc5b693679c5fb2be18237e8083
Magda Podgorska <podma@wp.pl>
2018-05-03 14:04:31 +0200

    package gry_losowe

---
+++ src/new_kalkulator/Kalkulator.java
@@ -0,0 +1,40 @@
+package new_kalkulator;
+
+import java.util.Scanner;
+
+public class Kalkulator {
+
+    private double liczba1;
+    private double liczba2;
+    private String znak;
+
+
+   public Kalkulator ()
+    {
+        for(;;) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Liczba1: ");
+            liczba1 = Integer.valueOf(scanner.nextLine());
+            System.out.print("Liczba2: ");
+            liczba2 = Integer.valueOf(scanner.nextLine());
+            System.out.print("rodzaj dzialania +,-,*,/: ");
+            znak = scanner.nextLine();
+            if (znak.equals("+")) {
+                System.out.println(liczba1 + liczba2);
+            } else if (znak.equals("-")) {
+                System.out.println(liczba1 - liczba2);
+            } else if (znak.equals("*")) {
+                System.out.println(liczba1 * liczba2);
+            } else if (znak.equals("/") & liczba2 == 0) {
+                System.out.println("Nie dziel przez zero");
+            } else if (znak.equals("/") & liczba2 != 0) {
+                System.out.println(liczba1 / liczba2);
+            }
+        }
+    }
+
+
+
+
+
+}
---
+++ src/new_kalkulator/Main.java
@@ -0,0 +1,13 @@
+package new_kalkulator;
+
+import static java.lang.Thread.sleep;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Kalkulator obiekt= new Kalkulator();
+
+
+   }
+}
